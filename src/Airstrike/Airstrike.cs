83ea2f9434625366eab52e9512e132af165ade7a
XxHarvzBackxX <Stampylongharv@gmail.com>
2021-12-29 22:11:29 +0000

    commit source

---
+++ src/Airstrike/Airstrike.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+
+namespace Airstrike
+{
+    internal class Airstrike
+    {
+        public Vector2 Position { get; set; } = Vector2.Zero;
+        public GameLocation Location { get; set; } = null;
+        public int Timer { get; set; } = 250;
+        public bool TickTimer { get; set; } = false;
+        public static List<Airstrike> Airstrikes { get; set; } = new List<Airstrike>();
+        public Airstrike(Vector2 position, GameLocation location)
+        {
+            Position = position;
+            Location = location;
+            Airstrikes.Add(this);
+            CallAirstrike();
+        }
+        public void Update()
+        {
+            if (TickTimer)
+            {
+                Timer -= 1;
+            }
+            if (Timer <= 0)
+            {
+                DoExplosion();
+                Airstrikes.Remove(this);
+            }
+        }
+        public void CallAirstrike()
+        {
+            Location.playSoundAt("furnace", Position);
+            Timer = 250;
+            TickTimer = true;
+        }
+        public void DoExplosion()
+        {
+            Location.playSoundAt("fireball", Position / Game1.tileSize);
+            Location.explode(Position / Game1.tileSize, 14, Game1.player, true, 45);
+        }
+    }
+}
---
+++ src/Airstrike/ModEntry.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Airstrike;
+using StardewModdingAPI;
+using StardewValley;
+
+/// <summary>The mod entry point.</summary>
+public class ModEntry : Mod
+{
+    /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+    /// <param name="helper">Provides simplified APIs for writing mods.</param>
+    public override void Entry(IModHelper helper)
+    {
+        Helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
+        Helper.Events.Input.ButtonsChanged += Input_ButtonsChanged;
+    }
+
+    private void Input_ButtonsChanged(object sender, StardewModdingAPI.Events.ButtonsChangedEventArgs e)
+    {
+        if (Context.IsWorldReady)
+        {
+            if (e.Released.Contains(SButton.MouseRight))
+            {
+                if (Game1.player.ActiveObject?.Name == "Airstrike Flare")
+                {
+                    if (!Game1.player.currentLocation.IsOutdoors)
+                    {
+                        Game1.addHUDMessage(new HUDMessage($"You need to be outside to call an airstrike!", HUDMessage.error_type));
+                        return;
+                    }
+                    else
+                    {
+                        if (Game1.player.Items[Game1.player.Items.IndexOf(Game1.player.ActiveObject)].Stack > 1)
+                        {
+                            Game1.player.Items[Game1.player.Items.IndexOf(Game1.player.ActiveObject)].Stack -= 1;
+                        }
+                        else
+                        {
+                            Game1.player.Items.Remove(Game1.player.ActiveObject);
+                        }
+                        Smoke smoke = new Smoke(e.Cursor.Tile, Game1.player.currentLocation);
+                    }
+                }
+            }
+        }
+    }
+
+    private void GameLoop_UpdateTicked(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
+    {
+        try
+        {
+            foreach (Smoke smoke in Smoke.Smokes)
+            {
+                smoke.Update();
+            }
+            foreach (Airstrike.Airstrike airstrike in Airstrike.Airstrike.Airstrikes)
+            {
+                airstrike.Update();
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
